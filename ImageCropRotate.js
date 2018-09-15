1030b1201ea9d90713e2fc4512b4d74ad555121a
Kate <yekaterina.filippova@gmail.com>
2018-09-14 22:04:55 -0400

    Add files via upload

--- ImageCropRotate.js
+++ ImageCropRotate.js
@@ -7,8 +7,65 @@ import { defaultImage } from './defaultImage.jpeg';
 export default class ImageCropRotate extends PureComponent {
 	state = {
 		file: "",
-        imagePreviewUrl: null
+        imagePreviewUrl: null,
+        rotationDegrees: 0,
+        hidden: true
 	}
+
+	onImageLoaded = (image) => {
+    	console.log("loaded")
+
+        const crop = 
+            makeAspectCrop({
+                x: 0,
+                y: 0,
+                aspect: 1,
+                width: 50,
+            }, image.width / image.height)
+
+        // crop returns percentages of the image, converting for pixel use
+        const data = {
+            crop,
+            width: Math.round(image.naturalWidth * (crop.width / 100)),
+            height: Math.round(image.naturalHeight * (crop.height / 100))
+        };
+
+        this.setState({
+            ...data,
+            hidden: false,
+            originalImgWidth: image.naturalWidth,
+            originalImgHeight: image.naturalHeight
+        });
+
+        getCroppedImg({
+            ...this.getImageStateAndProps(),
+            ...data,
+            degrees: this.state.rotationDegrees
+            },
+            true,
+            (dataUrl) => {
+                this.state.updateImage(dataUrl);
+            }
+        );
+    }
+
+    onChange = (crop, pixelCrop) => {
+        this.setState({
+            crop, 
+            height: pixelCrop.height,
+            width: pixelCrop.width,
+            x: pixelCrop.x,
+            y: pixelCrop.y
+        });
+
+        getCroppedImg(
+            this.getImageStateAndProps(),
+            true,
+            (dataUrl) => {
+                this.state.updateImage(dataUrl)
+            }
+        );
+    }
 
 	handleSubmit = (e) => {
 	    console.log("in the submit");
@@ -30,7 +87,25 @@ export default class ImageCropRotate extends PureComponent {
 	    reader.readAsDataURL(file)
 	}
 
+	updateImageRotation = (rotationDegrees) => {
+        this.setState({
+            rotationDegrees: (this.state.rotationDegrees + rotationDegrees) % 360
+        });
+    };
+
+	getBackgroundImage = () => {
+    	if (this.state.defaultImage != true) {
+            const style = {
+                backgroundImage: `url(${this.state.newImageUrl})`
+            }
+
+            return style;
+        }
+    }
+
 	render() {
+		const { crop, hidden } = this.state;
+
 		return (
 			<div>
 				<form onSubmit={(e)=>this.handleSubmit(e)}>
@@ -43,10 +118,35 @@ export default class ImageCropRotate extends PureComponent {
 	                      className="choose-file-button" />
 		            </label>
 	          	</form>
-	          	
+
+
+                  { !hidden &&
+
+                      <div className="button-container">
+                          <button onClick={() => this.updateImageRotation(-90)}>
+                              <i className="fa fa-rotate-left" />
+                              Rotate left
+                          </button>
+
+                          <button onClick={() => this.updateImageRotation(90)}>
+                              <i className="fa fa-rotate-right" />
+                              Rotate right
+                          </button>
+                      </div>
+                  }
+
 	          	<img src={this.state.imagePreviewUrl} />
+
+	          	<div className="image-preview-container" style={this.getBackgroundImage()}>
+                  <ReactCrop
+                      onChange={this.onChange}
+                      onImageLoaded={this.onImageLoaded}
+                      crop={crop}
+                      src={this.state.imagePickerUrl}
+                      keepSelection={true}
+                  />
+              </div>
           	</div>
 		)
 	}
 }
-
