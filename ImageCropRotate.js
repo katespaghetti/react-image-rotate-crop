a88546e9cd47e688c68d9b09cb28ebfb5234b5f8
Kate <yekaterina.filippova@gmail.com>
2018-10-01 22:39:09 -0400

    Add files via upload

--- ImageCropRotate.js
+++ ImageCropRotate.js
@@ -2,15 +2,22 @@ import React, { Component } from 'react';
 import ReactCrop, { makeAspectCrop } from 'react-image-crop';
 import 'react-image-crop/lib/ReactCrop.scss';
 import { getCroppedImg, getRotatedImg } from './cropRotateUtils';
-import { defaultImage } from './defaultImage.jpeg';
-import ImageWindow from './ImageWindow';
 
 export default class ImageCropRotate extends Component {
-	state = {
-		imagePreviewUrl: ""
-	}
+    state = {
+        hiddenButtons: true,
+        newImage: null,
+        imagePickerUrl: ''
+    };
 
-	onImageLoaded = (image) => {
+    getImageStateAndProps = () => {
+        return ({
+            ...this.state,
+            imagePreviewUrl: this.state.imagePickerUrl
+        })
+    }
+
+    onImageLoaded = (image) => {
         const crop = 
             makeAspectCrop({
                 x: 0,
@@ -28,26 +35,25 @@ export default class ImageCropRotate extends Component {
 
         this.setState({
             ...data,
+            hidden: false,
             originalImgWidth: image.naturalWidth,
             originalImgHeight: image.naturalHeight
         });
+
+        getCroppedImg({
+            ...this.getImageStateAndProps(),
+            ...data,
+            // degrees: this.props.profileInfo.rotationDegrees
+            },
+            true,
+            (dataUrl) => {
+                // this.props.updateProfileImage(dataUrl);
+                this.setState({
+                    newImage: dataUrl
+                })
+            }
+        );
     }
-
-    handleImageChange = (e) => {
-	    e.preventDefault();
-
-	    let reader = new FileReader();
-	    let file = e.target.files[0];
-
-	    reader.onloadend = () => {
-			this.setState({
-				file,
-				imagePreviewUrl: reader.result
-			});
-	    }
-
-	    reader.readAsDataURL(file)
-	}
 
     onChange = (crop, pixelCrop) => {
         this.setState({
@@ -57,35 +63,174 @@ export default class ImageCropRotate extends Component {
             x: pixelCrop.x,
             y: pixelCrop.y
         });
+
+        getCroppedImg(
+            this.getImageStateAndProps(),
+            true,
+            (dataUrl) => {
+                // this.props.updateProfileImage(dataUrl)
+                this.setState({
+                    newImage: dataUrl
+                })
+            }
+        );
     }
 
-	render() {
-		return (
-			<div>
-				<ImageWindow 
-					backgroundImage={this.state.imagePreviewUrl}
-				/>
+    toggleImageUploadPanel = () => {
+        this.setState({
+            ...this.state,
+            hidden: !this.state.hidden,
+            imagePickerUrl: '',
+            newImage: null 
+        })
+    }
 
-				<form onSubmit={(e)=>this.handleSubmit(e)}>
-	              	<label>
-	              		Choose Image
-		                <input
-	                      type="file"
-	                      accept="image/*"
-	                      onChange={(e)=>this.handleImageChange(e)}
-	                    />
-		            </label>
-	          	</form>
+    handleSubmit = (e) => {
+        e.preventDefault();
+        // const { uploadProfileImage, settings, profileInfo, toggleImageUploadPanel, uploadProfileImageButtonClick } = this.props;
 
-	          	<div>
-                  <ReactCrop
-                      onChange={this.onChange}
-                      onImageLoaded={this.onImageLoaded}
-                      crop={this.state.crop}
-                      src={this.state.imagePreviewUrl}
-                  />
-              </div>
-          	</div>
-		)
-	}
+        const { uploadProfileImage, settings, profileInfo, toggleImageUploadPanel } = this.props;
+
+        getCroppedImg({
+                ...this.getImageStateAndProps(),
+                degrees: profileInfo.rotationDegrees
+            },
+            false,
+            (croppedImgBlob) => {
+                // uploadProfileImage(api, settings.communityServiceUrl, croppedImgBlob);
+                // toggleImageUploadPanel();
+            }
+        )
+
+        // uploadProfileImageButtonClick();
+    }
+
+    handleImageChange = (e) => {
+        e.preventDefault();
+        const reader = new FileReader();
+        const file = e.target.files[0];
+
+        reader.onloadend = () => {
+            // this.props.setProfileImageUrl(reader.result);
+            this.setState({
+                imagePickerUrl: reader.result,
+                hiddenButtons: !this.state.hiddenButtons
+            })
+        }
+
+        reader.readAsDataURL(file)
+    }
+
+    rotate = (rotationDegrees) => {
+        const {originalImgWidth, originalImgHeight, imagePreviewUrl, imagePickerUrl } = this.state;
+
+        getRotatedImg(
+            {
+                imagePreviewUrl: imagePickerUrl,
+                width: originalImgWidth,
+                height: originalImgHeight,
+                degrees: this.props.profileInfo.rotationDegrees + rotationDegrees
+            },
+            (dataUrl) => {
+                // this.props.setProfileImageUrl(dataUrl);
+                this.setState({
+                    imagePickerUrl: dataUrl
+                })
+            } 
+        )
+    }
+
+    // edge will force a page refresh on cancel without this
+    cancelImageUpload = (e) => {
+        e.preventDefault();
+
+        this.setState({
+            ...this.state,
+            newImage: null,
+            hiddenButtons: true,
+            rotationDegrees: 0,
+            imagePickerUrl: ''
+        })
+    }
+
+    render() {
+        const { hiddenButtons, crop, newImage, imagePickerUrl } = this.state;
+
+        const style = {
+            width: 160,
+            height: 160,
+            minWidth: 160,
+            overflow: "hidden",
+            position: "relative",
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+            border: "1px solid #333",
+            backgroundImage: `url(${newImage})`
+        }
+
+        console.log("this is the state", this.state);
+
+        return (
+            <div className="image-upload-panel">
+                <div>
+                    <div style={style} />
+                </div>
+
+                <div>
+                    <div className="preview-component-container">
+                        <h3><strong>Upload an image</strong></h3>
+
+                        <form onSubmit={(e)=>this.handleSubmit(e)}>
+                            <label>
+                            Choose Image
+                                <input
+                                    type="file"
+                                    accept="image/*"
+                                    onChange={(e)=>this.handleImageChange(e)}
+                                />
+                            </label>
+                            <button 
+                                type="submit" 
+                                onClick={(e)=>this.cancelImageUpload(e)}>
+                                Cancel
+                            </button>
+
+                            {!hiddenButtons && 
+                                <button 
+                                    type="submit" 
+                                    onClick={(e)=>this.handleSubmit(e)}>
+                                    Upload
+                                </button>
+                            }
+                        </form>
+
+                        { !hiddenButtons &&
+
+                            <div>
+                                <button onClick={()=>this.rotate(-90)}>
+                                    <i className="fa fa-rotate-left" />
+                                    Rotate left
+                                </button>
+
+                                <button onClick={()=>this.rotate(90)}>
+                                    <i className="fa fa-rotate-right" />
+                                    Rotate right
+                                </button>
+                            </div>
+                        }
+
+                        <div>
+                            <ReactCrop
+                                onChange={this.onChange}
+                                onImageLoaded={this.onImageLoaded}
+                                crop={crop}
+                                src={imagePickerUrl}
+                                keepSelection={true}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
